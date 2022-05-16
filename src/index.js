8c25d53c5077d46255c159a19f8edaf7659a01bd
unknown <Nguyen Tien Dung>
2022-05-16 18:20:27 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+
+import App from './App';
+
+import './global.css';
+
+ReactDom.render(
+  
+    
+      <App />
+    
+  
+  
+).document.getElementById('root');
+
