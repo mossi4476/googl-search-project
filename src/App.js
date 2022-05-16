f32ca9f8486d40429bdeec87e181f0e4070f2812
Nguyen Tien Dung <sparkroy.113@gmail.com>
2022-05-16 22:05:13 +0700

    routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { Footer } from "./components/Footer";
+import { Navbar } from "./components/Navbar";
+import { Routes } from "./components/Routes";
 
 const App = () => {
+  const [darkTheme, setDarkTheme] = useState(false);
+
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className={darkTheme ? "dark" : ""}>
+      <div className="dark:bg-gray-900 bg-gray-100 dark:text-gray-200 black min-h-screen">
+        <Navbar setDarkTheme={setDarkTheme} darkTheme={darkTheme} />
+        <Routes />
+        <Footer />
+      </div>
     </div>
   );
 };
